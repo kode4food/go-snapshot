33c855ba0e08b47d5161a2be163f43a82de97daf
Thomas Bradford <thom@kode4food.it>
2017-05-21 23:14:00 +0200

    first cut

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # go-snapshot
-Simple asset file generator for Golang
+Simple asset file generator for Golang. No recursion, no file system interface, no restore. Just generates a big map of byte arrays based on the globs you provide.
+
+## How To Install
+Make sure your `GOPATH` is set, then run `go get` to retrieve the
+package.
+
+```bash
+go get github.com/kode4food/go-snapshot
+```
+
+## How To Use
+Once you've installed the package, you can run it from `GOPATH/bin`
+like so:
+
+```bash
+go-snapshot ./dir1/*.md ./dir2/*.lisp -out ./assets/snapshot.go
+```
+
---
+++ main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+type config struct {
+	pkg string
+	out string
+	in  []string
+}
+
+type filedata map[string]string
+
+func main() {
+	c := parseConfig()
+	validateConfig(c)
+	d := read(c)
+	write(c, d)
+}
+
+func parseConfig() *config {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options] <input directories>\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	c := &config{}
+	flag.StringVar(&c.pkg, "pkg", c.pkg, "Package name for generated code.")
+	flag.StringVar(&c.out, "out", c.out, "Output file to be generated.")
+	flag.Parse()
+
+	c.in = make([]string, flag.NArg())
+	for i := range c.in {
+		c.in[i] = flag.Arg(i)
+	}
+
+	return c
+}
+
+func validateConfig(c *config) {
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "Missing <input dir>\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if c.out == "" || c.pkg == "" {
+		fmt.Fprintf(os.Stderr, "Package and output file are required\n\n")
+		flag.Usage()
+		os.Exit(2)
+	}
+}
+
+func read(c *config) filedata {
+	d := filedata{}
+	for _, pattern := range c.in {
+		readPattern(d, pattern)
+	}
+	return d
+}
+
+func readPattern(d filedata, pattern string) {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		panic(fmt.Errorf("couldn't resolve pattern %s", pattern))
+	}
+	for _, filename := range matches {
+		readFile(d, filename)
+	}
+}
+
+func readFile(d filedata, filename string) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Errorf("couldn't read from %s", filename))
+	}
+	d[filename] = compress(b)
+}
+
+func compress(b []byte) string {
+	var buf bytes.Buffer
+	w := gzip.NewWriter(&buf)
+	w.Write(b)
+	w.Close()
+
+	s := fmt.Sprintf("%x", buf.Bytes())
+	p := make([]string, len(s)/2)
+	for i, j := 0, 0; i < len(s); i += 2 {
+		p[j] = s[i : i+2]
+		j++
+	}
+	return `\x` + strings.Join(p, `\x`)
+}
+
+func write(c *config, d filedata) {
+	var buf bytes.Buffer
+	buf.WriteString(header(c))
+	buf.WriteString(filenames(d))
+	buf.WriteString(data(d))
+	buf.WriteString(functions())
+	ioutil.WriteFile(c.out, buf.Bytes(), os.ModePerm)
+}
+
+func header(c *config) string {
+	return fmt.Sprintf(`
+package %s
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+)
+`, c.pkg) + "\n"
+}
+
+func filenames(d filedata) string {
+	var buf bytes.Buffer
+	buf.WriteString("var Filenames = []string{\n")
+	for _, f := range sortedFilenames(d) {
+		buf.WriteString(fmt.Sprintf("\t%q,\n", f))
+	}
+	buf.WriteString("}\n\n")
+	return buf.String()
+}
+
+func sortedFilenames(d filedata) []string {
+	fn := make([]string, len(d))
+	i := 0
+	for f := range d {
+		fn[i] = f
+		i++
+	}
+	sort.Strings(fn)
+	return fn
+}
+
+func data(d filedata) string {
+	var buf bytes.Buffer
+	buf.WriteString("var Data = map[string][]byte{\n")
+	for fn, data := range d {
+		buf.WriteString(fmt.Sprintf("\t%q: []byte(\"%s\"),\n", fn, data))
+	}
+	buf.WriteString("}\n")
+	return buf.String()
+}
+
+func functions() string {
+	return `
+func Get(fn string) ([]byte, bool) {
+	if d, ok := Data[fn]; ok {
+		return uncompress(d), true
+	}
+	return nil, false
+}
+
+func MustGet(fn string) []byte {
+	if r, ok := Get(fn); ok {
+		return r
+	}
+	panic(fmt.Errorf("could not find asset: %s", fn))
+}
+
+func uncompress(b []byte) []byte {
+	r, err := gzip.NewReader(bytes.NewBuffer(b))
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.Bytes()
+}`
+}
